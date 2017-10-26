a5a62a1440e8861177a6f4562e1e13b479e1fa79
mcheng3 <michael.cheng65@gmail.com>
2017-10-25 22:09:02 -0400

    Implement login and join

--- ourStory.py
+++ ourStory.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, render_template, request, session, redirect, url_for
 import sqlite3
+import util.users as users
 
 
 app = Flask(__name__)
@@ -8,28 +9,47 @@ app.secret_key = "THIS IS NOT SECURE"
 @app.route('/')
 def home():
     if 'user' in session:
-                return render_template("input.html", user = session['user'], method = request.method)
+        return render_template("input.html", user = session['user'], method = request.method)
     else:
-	        return render_template('form.html', title = 'Main')
+        return render_template('form.html', title = 'Main')
 
 @app.route('/help')
 def help():
     return
 
-
 @app.route('/login', methods=['POST','GET'])
 def login():
-    print request.form['user']
+    db = sqlite3.connect("data/ourDB.db") #opens ourDB.db
+    c = db.cursor() #opens a cursor object
+    #print request.form['user']
     user = request.form['user']
-    if user == 'bob':
-        if request.form['password'] == 'secret':
-            session['user'] = request.form['user']
-            return redirect(url_for('stories'))
-        else:
-            flash("Sorry, wrong password")
+    command = "SELECT password FROM users WHERE username='%s';"%(user)
+    c.execute(command)
+    credentials = c.fetchall()
+    password = credentials[0][0]
+    if request.form['password'] == password:
+        session['user'] = request.form['user']
+        return redirect(url_for('stories'))
     else:
-        flash("Sorry, wrong username")
-	return redirect(url_for('home'))
+        flash("Sorry, wrong password")
+    return redirect(url_for('home'))
+
+@app.route('/join')
+def join():
+    return render_template("join.html")
+
+@app.route('/joinRedirect', methods=['POST','GET'])
+def joinRedirect():
+    db = sqlite3.connect("data/ourDB.db") #opens ourDB.db
+    if request.form['password'] == request.form['passwordConfirm']:
+        users.add_new_user(db, request.form['user'], request.form['password'], request.form['name'])
+        db.commit()
+        session['user'] = request.form['user']
+        return redirect(url_for('stories'))
+    else:
+        flash("Passwords do not match")
+    return redirect(url_for('join'))
+    
 
 @app.route('/stories')
 def stories():
@@ -57,5 +77,5 @@ def logout():
         return redirect(url_for('home'))
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
--- util/users.py
+++ util/users.py
@@ -1,34 +1,37 @@
 import sqlite3 #enables control of an sqlite database
 
-db = sqlite3.connect("ourDB.db") #opens ourDB.db
-c = db.cursor() #opens a cursor object
+#db = sqlite3.connect("data/ourDB.db") #opens ourDB.db
+#cursor = db.cursor() #opens a cursor object
 
 #-------------------------------------------------------------
 
 #Run once, creates the table
-def create_table():
+def create_table(c):
     c.execute("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT, full_name TEXT, likes TEXT);")
 
 #Adds a new record to the users table    
-def add_new_user(user, pw, name):
-    command = "INSERT INTO users VALUES(%s,%s,%s);"%(user,pw,name)
+def add_new_user(db, user, pw, name):
+    c = db.cursor()
+    command = "INSERT INTO users VALUES ('%s', '%s', '%s', '0');"%(user,pw,name)
     c.execute(command)
+    print 'done'
 
 #Updates a user's username to a new one    
-def update_username(old_user, new_user):
+def update_username(c, old_user, new_user):
     command = "UPDATE users SET username = %s WHERE username = %s;" %(new_user,old_user)
     c.execute(command)
 
 #Updates a user's password to a new one    
-def update_password(user, old_pass, new_pass):
+def update_password(c, user, old_pass, new_pass):
     command = "UPDATE users SET password = %s WHERE password = %s AND username = %s;" %(new_pass,old_pass,user)
     c.execute(command)
 
 #Updates a user's fullname to a new one    
-def update_fullname(user, old_name, new_name):
+def update_fullname(c, user, old_name, new_name):
     command = "UPDATE users SET fullname = %s WHERE fullname = %s AND username = %s;" %(new_name,old_name,user)
     c.execute(command)
 
 #Given a username, will retrieve the user's password and fullname
-def get_user_info(user):
+def get_user_info(c, user):
     command = "SELECT password,fullname FROM users WHERE username = %s;" %(user) 
+
