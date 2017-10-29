87d7a08b5e1761bee8792c3ea44118d31649e033
soe43 <soe433@gmail.com>
2017-10-29 12:33:17 -0400

    closed and commited db

--- util/search.py
+++ util/search.py
@@ -121,7 +121,10 @@ def getStory(story_id):
     command = "SELECT author, title, genre, finished, popularity, views, contributions, word_limit FROM stories WHERE id = %s;"%(story_id)
     list_of_attributes = c.execute(command).fetchall()
     storydict = tuple_to_dictionary(list_of_attributes, ["%s"%(story_id),"author", "title", "genre", "finished", "popularity", "views", "contributions", "cooldown", "word_limit", piecesdict])
+    db.commit()
+    db.close()
     return storydict
+
     
     
 
